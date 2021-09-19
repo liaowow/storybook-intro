610bc9942fa8cb626ff904c505e1d492a4d55fc4
Annie Liao <liaowow@gmail.com>
2021-09-19 15:16:00 -0400

    specify data requirement via PropTypes

--- taskbox/src/components/Task.js
+++ taskbox/src/components/Task.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 export default function Task({ task: { id, title, state }, onArchiveTask, onPinTask }) {
   return (
     <div className={`list-item ${state}`}>
@@ -26,4 +26,14 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
       </div>
     </div>
   )
+}
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
 }
